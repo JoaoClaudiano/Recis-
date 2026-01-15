3b7b222bbe28c36a2bfa29a2d5b33ca76bcd5fb0
João Claudiano <46274604+JoaoClaudiano@users.noreply.github.com>
2026-01-15 11:26:55 -0300

    Criar sw.js

---
+++ sw.js
@@ -0,0 +1,52 @@
+const CACHE_NAME = 'rescisao-calc-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/sobre.html',
+  '/contato.html',
+  '/politica-privacidade.html',
+  '/css/style.css',
+  '/icons/icon-72.png',
+  '/icons/icon-192.png',
+  '/icons/icon-512.png'
+];
+
+// Install event
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Activate event
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Fetch event
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
